f1d642119e1596739f17550177eedb8fa3655085
Jafet Suárez <jafet.suarezxbox@hotmail.com>
2021-06-13 18:17:43 -0600

    :D

---
+++ README.md
@@ -0,0 +1,11 @@
+# Best Smiles
+Very quick example about launch pre-trained TensorFlow.js model with Python on Windows.
+
+## Launch the Best Smiles web page
+Now you are ready to...
+```bash
+py -m http.server [<portNo>]
+```
+
+## Mic and Cam
+Don't forget to turn on these peripherals. Enjoy!
---
+++ index.js
@@ -0,0 +1,122 @@
+//https://developer.mozilla.org/es/docs/Web/API/WebRTC_API/Taking_still_photos
+(function () {
+  var streaming = false,
+    video = document.querySelector("#video"),
+    canvas = document.querySelector("#canvas"),
+    photo = document.querySelector("#photo"),
+    startbutton = document.querySelector("#startbutton"),
+    width = 320,
+    height = 0;
+
+  navigator.getMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia;
+
+  navigator.getMedia(
+    {
+      video: true,
+      audio: false,
+    },
+    function (stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        //video.src = vendorURL.createObjectURL(stream);
+        video.srcObject = stream;
+      }
+      video.play();
+    },
+    function (err) {
+      console.log("An error occured! " + err);
+    }
+  );
+
+  video.addEventListener(
+    "canplay",
+    function (ev) {
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth / width);
+        video.setAttribute("width", width);
+        video.setAttribute("height", height);
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+        streaming = true;
+      }
+    },
+    false
+  );
+
+  function takepicture() {
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext("2d").drawImage(video, 0, 0, width, height);
+    var data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+  }
+
+  /*
+	startbutton.addEventListener('click', function(ev){
+		takepicture();
+		ev.preventDefault();
+	}, false);
+*/
+
+  //https://codelabs.developers.google.com/codelabs/tensorflowjs-audio-codelab/index.html
+  //"zero" to "nine", "up", "down", "left", "right", "go", "stop", "yes", "no", as well as the additional categories of "unknown word" and "background noise"
+  let recognizer;
+
+  function predictWord() {
+    // Array of words that the recognizer is trained to recognize.
+    const words = recognizer.wordLabels();
+    recognizer.listen(
+      ({ scores }) => {
+        // Turn scores into a list of (score,word) pairs.
+        scores = Array.from(scores).map((s, i) => ({
+          score: s,
+          word: words[i],
+        }));
+        // Find the most probable word.
+        scores.sort((s1, s2) => s2.score - s1.score);
+        /*
+		document.querySelector("#console").textContent = scores[0].word;
+        if (scores[0].word === "go") {
+			console.log(scores[0].word);
+		}
+	  	*/
+        switch (scores[0].word) {
+          case "one":
+            // code block
+            document.querySelector("#counter").textContent = "1️⃣";
+            break;
+          case "two":
+            // code block
+            document.querySelector("#counter").textContent = "2️⃣";
+            break;
+          case "three":
+            // code block
+            document.querySelector("#counter").textContent = "😁";
+            takepicture();
+            break;
+          //default:
+          // code block
+        }
+      },
+      { probabilityThreshold: 0.75 }
+    );
+  }
+
+  async function app() {
+    recognizer = speechCommands.create("BROWSER_FFT");
+    await recognizer.ensureModelLoaded();
+    predictWord();
+    let element = document.getElementById("loader");
+    element.classList.add("hide-loader");
+    document.getElementById("title").innerHTML =
+      "Count until three to take a photo! 📷";
+  }
+
+  app();
+})();
